342f98b9384d5dff6503d85546d7c1c6382fcdf8
yingtzu <ennie920203@gmail.com>
2023-03-20 11:20:39 +0800

    start_end

--- num.py
+++ num.py
@@ -3,7 +3,9 @@
 #猜對了，印出"你答對了"
 #猜錯了，要告訴他比答案大還是小
 import random
-r = random.randint(1, 100)
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start, end)
 count = 0
 while True:
     count = count + 1
